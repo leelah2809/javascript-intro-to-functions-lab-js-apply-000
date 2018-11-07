42116eda41e8fdb6662895e0efdee7adc5070243
Okunpls Jelilat Dasola <jelilatokunola2809@gmail.com>
2018-11-07 08:08:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,11 @@ function sayHiToGrandma(string){
   {
   return ('I can\'t hear you!')}
   else if
-  (string === {
+  (string === 'I can\'t hear you!'){
     return ('YES INDEED!')
+  }
+  else{
+    return ('I love you, too.')
   }
   
 }
